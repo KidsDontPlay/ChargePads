5b4420550398fcfc9f9d8523cd948fcff463dbbb
MrRiegel <ichhabads@gmail.com>
2016-10-12 21:12:50 +0200

    hotfix

--- src/main/java/mrriegel/chargepads/ChargePads.java
+++ src/main/java/mrriegel/chargepads/ChargePads.java
@@ -9,7 +9,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = ChargePads.MODID, name = ChargePads.MODNAME, version = ChargePads.VERSION, dependencies = "required-after:LimeLib@[1.0.1,)")
+@Mod(modid = ChargePads.MODID, name = ChargePads.MODNAME, version = ChargePads.VERSION, dependencies = "required-after:limelib@[1.0.1,)")
 public class ChargePads {
 	public static final String MODID = "chargepads";
 	public static final String VERSION = "1.0.0";
--- src/main/java/mrriegel/chargepads/proxy/CommonProxy.java
+++ src/main/java/mrriegel/chargepads/proxy/CommonProxy.java
@@ -10,7 +10,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-public class CommonProxy implements IProxy {
+public class CommonProxy implements IProxy{
 
 	@Override
 	public void preInit(FMLPreInitializationEvent event) {
